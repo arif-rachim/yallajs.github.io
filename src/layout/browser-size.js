4d2d7f1ceddd7b379baa1052049355109ccb50a3
arif <a.arif.r@gmail.com>
2017-03-11 19:11:45 +0400

    Initial commit

---
+++ src/layout/browser-size.js
@@ -0,0 +1,11 @@
+/**
+ * Created by developer on 3/11/2017.
+ */
+
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+$export = {
+    width : w,
+    height : h
+}
--- src/layout/content.js
+++ src/layout/content.js
@@ -3,11 +3,15 @@
  */
 var container = $inject('components/ui-container');
 var item = $inject('components/ui-container-item');
-
+var browserSize = $inject('layout/browser-size');
 var _scrollLastYPos = 0;
 var _showFull = false;
-var _heroHeight = 400;
+var _heroHeight = browserSize.height * 0.6;
+var _heroMinHeight = 50;
+
 var _transitionSpeed = '0.5s';
+
+
 function $render(props) {
 
     var contentData = ['div',{
@@ -20,11 +24,11 @@ function $render(props) {
     var content = ['div',{
         style : {
             position : 'absolute',
-            top : _showFull ? '0px' : _heroHeight+'px',
+            top : _showFull ? _heroMinHeight+'px' : (_heroHeight)+'px',
             left : '0',
             right : '0',
             bottom : _showFull ? '0px' : '30px',
-            backgroundColor : '#CCCCCC',
+            backgroundColor : 'white',
             overflow : 'auto',
             transition : _transitionSpeed
         },
@@ -45,16 +49,19 @@ function $render(props) {
         },100,false)
     },contentData];
 
-    return ['div',content,['div',{
+    var header = ['div',{
         style : {
             position : 'absolute',
-            top : _showFull ? '-'+_heroHeight+'px' : '0px',
+            top : _showFull ? '-'+(_heroHeight-_heroMinHeight)+'px' : '0px',
             left : '0',
             right : '0',
             height : _heroHeight+'px',
-            boxShadow : '0px 0px 5px 0px rgba(0,0,0,0.5)',
+            backgroundColor : _showFull ? '#CCCCCC' : '#EFEFEF',
+            boxShadow : '0px 0px 10px 0px rgba(0,0,0,0.5)',
             transition : _transitionSpeed
         }
-    },'header']]
+    },'header'];
+
+    return ['div',content,header]
 
 }
