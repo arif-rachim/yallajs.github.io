573a8a7bc820b7ad2e86c3505734d617a83646e9
arif <a.arif.r@gmail.com>
2017-03-11 11:08:23 +0400

    Initial commit

--- src/layout/content.js
+++ src/layout/content.js
@@ -4,74 +4,65 @@
 var container = $inject('components/ui-container');
 var item = $inject('components/ui-container-item');
 
-var _basis = 300;
-
+var _scrollLastYPos = 0;
+var _showFull = false;
+var _heroHeight = 200;
 
 function $render(props) {
 
-    var header = [item, {
-        basis: _basis+'px',
-        grow: '0',
-        style: {
-            backgroundColor: 'white',
-            position : 'relative'
+    var contentData = ['div',{
+        style : {
+            height : '3000px'
         }
-    }, 'shit'];
 
-    var footer = [item, {
-        grow: '0',
-        basis: 'auto',
-        style: {
-            backgroundColor: 'blue'
-        }
-    }, 'shit three'];
+    }
+    ,'Content'];
 
-
-    var content = [item, {
-        style: {
-            position: 'relative'
-        }
-    }, ['div', {
-        style: {
-            position: 'absolute',
-            top: '0',
-            left: '0',
-            right: '0',
-            bottom: '0',
-            backgroundColor: 'red',
-            overflow: 'auto'
+    var content = ['div',{
+        style : {
+            position : 'absolute',
+            top : _showFull ? '0px' : _heroHeight+'px',
+            left : '0',
+            right : '0',
+            bottom : _showFull ? '0px' : '30px',
+            backgroundColor : '#CCCCCC',
+            overflow : 'auto',
+            transition : '0.5s'
         },
         onscroll : function(e){
-            resetTopPos(e.target.scrollTop);
+            var topPos = e.target.scrollTop;
+            if(_scrollLastYPos > topPos){
+                // its moving up
+                console.log('scroll move up');
+                if(_showFull){
+                    console.log('We should not show full');
+                    _showFull = false;
+                    yalla.markAsDirty();
+                }
+            }else{
+                // its moving down
+                console.log('scroll move down');
+                if(topPos > _heroHeight &&  !_showFull){
+                    console.log('We should show full');
+                    _showFull = true;
+                    yalla.markAsDirty();
+                }
+            }
+            _scrollLastYPos = topPos;
+            console.log(_scrollLastYPos);
         }
-    }, ['div', {
-        style: {
-            height: '1000px'
-        }
-    }, 'shit']]];
+    },contentData];
 
-    var shell = [container, {
-        direction: 'column',
-        style: {
-            width: '100vw',
-            height: '100vh'
+    return ['div',content,['div',{
+        style : {
+            position : 'absolute',
+            top : _showFull ? '-'+_heroHeight+'px' : '0px',
+            left : '0',
+            right : '0',
+            height : _heroHeight+'px',
+            boxShadow : '0px 0px 5px 0px rgba(0,0,0,0.5)',
+            transition : '0.5s'
         }
-    }, header, content, footer];
+    },'header']]
 
-    return ['div', {
-        style: {
-            position: 'fixed',
-            top: '0',
-            left: '0',
-            width: '100vw',
-            height: '100vh',
-            backgroundColor: '#DDD',
-            overflow: 'auto'
-        }
-    }, shell]
-}
-
-function resetTopPos(topPos){
-    _basis = 300 - topPos;
-    yalla.markAsDirty();
 }
