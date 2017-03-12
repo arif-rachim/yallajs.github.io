cdaf30f35d73d7cb9bc5f908269781a97629e279
arif <a.arif.r@gmail.com>
2017-03-12 15:22:54 +0400

    Initial commit

--- src/layout/content.js
+++ src/layout/content.js
@@ -17,7 +17,7 @@ function $render() {
             style: containerStyle('row')
         }, ['div', {
             style: contentStyle(true)
-        }, ['div', 'Easy to learned, because yallajs is using the concept of stateless functional component combined with unidirectional flow state container.Stateless functional components (SFC) have very attractive features, its easy to understand because its just a pure function that can accept input and provide output based on the input without mutating the data or causing side effect. SFC is generally only used for rendering dumb components such as a container.However if an SFC given a state container that can manage data in a unidirectional flow, it can be turned into a smart components without losing the beauty of a stateless components.']],
+        }, ['div', 'Easy to learn, because yallajs is using the concept of stateless functional component combined with unidirectional flow state container.Stateless functional components (SFC) have very attractive features, its easy to understand because its just a pure function that can accept input and provide output based on the input without mutating the data or causing side effect. SFC is generally only used for rendering dumb components such as a container.However if an SFC given a state container that can manage data in a unidirectional flow, it can be turned into a smart components without losing the beauty of a stateless components.']],
             ['div', {
                 style: contentStyle(false)
             }, [icon,{icon : 'school',size : '120px'}]]],
