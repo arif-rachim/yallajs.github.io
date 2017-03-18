67cb8f96a19b61929e1f8f7ce2feb33e257d1a84
arif <a.arif.r@gmail.com>
2017-03-18 09:56:31 +0400

    Initial commit

---
+++ src/site/browser-size.js
@@ -0,0 +1,12 @@
+/**
+ * Created by developer on 3/18/2017.
+ */
+
+$export = {
+    getWidth : function () {
+        return window.innerWidth || document.body.clientWidth;
+    },
+    getHeight : function(){
+        return window.innerHeight || document.body.clientHeight;
+    }
+}
